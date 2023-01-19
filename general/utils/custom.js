b738ce2ec92d257461064f4e98cb5072a6898bf6
mkhojasteh7201 <mkhojasteh29995@gmail.com>
2023-01-19 20:05:29 +0330

    end Login

--- general/utils/custom.js
+++ general/utils/custom.js
@@ -66,7 +66,7 @@ module.exports = {
         return dateNow;
     },
 
-    async userRegister(data , clientIp , clientPort){
+    async   userRegister(data , clientIp , clientPort){
         
        
     let jsonGuard = {};
